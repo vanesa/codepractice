d41c72843249f59419404f98c43ad6fde115c089
Vanesa <vanesa@gmx.net>
2015-10-21 23:43:11 -0700

    first commit

---
+++ hackbright/skills-lists/skills.py
@@ -0,0 +1,286 @@
+"""Skills Assessment: Lists.
+
+To work on an item, uncomment the entire function (including the docstring)
+and then run this script. It should output an error that the newly-uncommented
+function is now failing its tests.
+
+Edit the function body until you have a solution and the test passes, and then
+move on to the next function.
+
+This assessment is DUE TO YOUR ADVISOR BY SUNDAY EVENING.
+"""
+
+
+def all_odd(number_list):
+    """Return a list of only the odd numbers in the input list.
+
+        >>> all_odd([1, 2, 7, -5])
+        [1, 7, -5]
+
+        >>> all_odd([2, -6, 8])
+        []
+
+    """
+    odd_nums = []
+    for i in number_list:
+        if i % 2 != 0:
+            odd_nums.append(i)
+
+    return odd_nums
+
+
+def all_even(number_list):
+    """Return a list of only the even numbers in the input list.
+
+        >>> all_even([2, 6, -1, -2])
+        [2, 6, -2]
+
+        >>> all_even([-1, 3, 5])
+        []
+
+    """
+
+    even_nums = []
+    for i in number_list:
+        if i % 2 == 0:
+            even_nums.append(i)
+
+    return even_nums
+
+
+def print_indeces(my_list):
+    """Print the index of each list item, followed by the item itself.
+    Do this without using a counting variable. I.e. don't do something
+    like this:
+
+    count = 0
+    for item in list:
+        print count
+        count = count + 1
+
+    Output should look like this:
+
+    >>> print_indeces(["Toyota", "Jeep", "Volvo"])
+    0 Toyota
+    1 Jeep
+    2 Volvo
+
+    """
+    for i in range(len(my_list)):
+        print "%s %s" % (i, my_list[i])
+
+
+def long_words(word_list):
+    """Return all words in input list that are longer than 4 characters.
+
+        >>> long_words(["hello", "hey", "spam", "spam", "bacon", "bacon"])
+        ['hello', 'bacon', 'bacon']
+
+        >>> long_words(["all", "are", "tiny"])
+        []
+
+    """
+    long_words = []
+    for word in word_list:
+        if len(word) > 4:
+            long_words.append(word)
+    return long_words
+
+
+def smallest_int(number_list):
+    """Find the smallest integer in a list of integers and return it.
+
+        >>> smallest_int([-5, 2, -5, 7])
+        -5
+
+    If the input list is empty, return None:
+
+        >>> smallest_int([]) is None
+        True
+
+    """
+    smallest = 0
+    while number_list:
+        for num in number_list:
+            if num < smallest:
+                smallest = num
+        return smallest
+    return None
+
+def largest_int(number_list):
+    """Find the largest integer in a list of integers and return it.
+
+        >>> largest_int([-5, 2, -5, 7])
+        7
+
+    If the input list is empty, return None:
+
+        >>> largest_int([]) is None
+        True
+
+    """
+
+    largest = 0
+    while number_list:
+        for num in number_list:
+            if num > largest:
+                largest = num
+        return largest
+    return None
+
+
+def halvesies(number_list):
+    """Return list of numbers from input list, each divided by two.
+
+        >>> halvesies([2, 6, -2])
+        [1.0, 3.0, -1.0]
+
+    If any of the numbers are, make sure you don't round off the half:
+
+        >>> halvesies([1, 5])
+        [0.5, 2.5]
+
+    """
+    halvies = []
+    for num in number_list:
+        halvies.append(float(num)/2)
+    return halvies
+
+
+def word_lengths(word_list):
+    """Return the length of words in the input list.
+
+        >>> word_lengths(["hello", "hey", "hello", "spam"])
+        [5, 3, 5, 4]
+
+    """
+    word_len = []
+    for word in word_list:
+        word_len.append(len(word))
+
+    return word_len
+
+
+def sum_numbers(number_list):
+    """Return the sum of all of the numbers in the list.
+
+    Python has a built-in function, `sum()`, which already does this -- but for
+    this exercise, you should not use it.
+
+        >>> sum_numbers([1, 2, 3, 10])
+        16
+
+    Any empty list should return the sum of zero:
+
+        >>> sum_numbers([])
+        0
+
+    """
+    sumofnums = 0
+    while number_list:
+        for num in number_list:
+            sumofnums += int(num)
+        return sumofnums
+    return 0
+
+def mult_numbers(number_list):
+    """Return product (result of multiplication) of the numbers in the list.
+
+        >>> mult_numbers([1, 2, 3])
+        6
+
+    Obviously, if there is a zero in the input, the product will be zero:
+
+        >>> mult_numbers([10, 20, 0, 50])
+        0
+
+    As explained at http://en.wikipedia.org/wiki/Empty_product, if the list is
+    empty, the product should be 1:
+
+        >>> mult_numbers([])
+        1
+
+    """
+
+    multofnums = 1
+    while number_list:
+        for num in number_list:
+            multofnums *= int(num)
+        return multofnums
+    return 1
+
+
+def join_strings(word_list):
+    """Return a string of all input strings joined together.
+
+    Python ha a built-in method on lists, `join` -- but this exercise, you
+    should not use it.
+
+        >>> join_strings(["spam", "spam", "bacon", "balloonicorn"])
+        'spamspambaconballoonicorn'
+
+    For an empty list, you should return an empty string:
+
+        >>> join_strings([])
+        ''
+
+    """
+    
+    joined_string = ''
+    while word_list:
+        for word in word_list:
+            joined_string = joined_string + word
+        return joined_string
+    return ''
+
+
+def average(number_list):
+    """Return the average (mean) of the list of numbers given.
+
+        >>> average([2, 12, 3])
+        5.666666666666667
+
+    There is no defined answer if the list given is empty. It's fine if
+    this raises an error when given an empty list.
+    """
+    sumnums = 0
+    for num in number_list:
+        sumnums += float(num)
+    average_num = sumnums / len(number_list)
+    return average_num
+
+
+#############################################################################
+# END OF SKILLS TEST: You can stop here, or read on to work on advanced problem.
+
+# Uncomment the function below to work on the advanced problem.
+# Tip: To comment or uncomment blocks of code, highlight what you want to
+#    comment or uncomment, and then CMD+"/" or CTRL-"/"
+
+# def advanced_join_strings(list_of_words):
+#     """Return a single string with each word from the input list
+#     separated by a comma.
+
+#         >>> advanced_join_strings(["Labrador", "Poodle", "French Bulldog"])
+#         'Labrador, Poodle, French Bulldog'
+
+#     If there's only one thing in the list, it should return just that
+#     thing, of course
+
+#         >>> advanced_join_strings(["Pretzel"])
+#         'Pretzel'
+
+#     """
+
+#     return ""
+
+# END OF ASSIGNMENT: You can ignore everything below.
+##############################################################################
+
+if __name__ == "__main__":
+    import doctest
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "*** %s TESTS PASSED. GOOD WORK!" % result.attempted
+    print
---
+++ hackerrank/pythonintro.py
@@ -0,0 +1,320 @@
+from __future__ import division
+import sys
+
+"""
+HACKERRANK CHALLENGES - PYTHON INTRODUCTION
+"""
+
+
+#####Arithmetic Operators#####
+
+"""
+
+Task 
+Read two integers from STDIN and print three lines where: 
+(1) the first line contains the sum of the two numbers, 
+(2) the second line contains the difference of the two numbers (first - second), 
+(3) the third line contains the product of the two numbers.
+
+Input Format 
+The first line contains the first integer, a, and the second line contains the second integer, b.
+
+Constraints 
+1<=a<=1010 
+1<=b<=1010
+
+Output Format 
+Print three lines as explained above.
+
+Sample Input
+
+3
+2
+Sample Output
+
+5
+1
+6
+Explanation 
+3 plus 2=5 
+3 minus 2=1 
+3 times 2=6
+
+"""
+
+def hr_arithmetic():
+	def calculations(a,b):
+	    results = []
+	    results.append(a + b)
+	    results.append(a - b)
+	    results.append(a * b)
+	    return results
+	    
+	a = int(raw_input("Type one integer: "))
+	b = int(raw_input("Type one integer: "))
+	res = calculations(a,b)
+	print res[0]
+	print res[1]
+	print res[2]
+
+
+####DIVISION####
+
+"""
+Task 
+Read two integers and print two lines, such that the first line contains integer division and the second line contains float division.
+
+You don't need to perform any rounding or formatting operation.
+
+Input Format 
+The first line contains the first integer, a, and the second line contains the second integer, b.
+
+Output Format 
+Print two lines as described above.
+
+Sample Input
+
+4
+3
+sample Output
+
+1
+1.3333333333333333
+"""
+
+def hr_division():
+	#from __future__ import division
+
+	a = int(raw_input("Type one integer: "))
+	b = int(raw_input("Type one integer: "))
+
+	print a//b
+	print a/b
+
+
+####Mod Divmod####
+
+"""
+Task 
+Read two integers, a and b, and print three lines. 
+The first line is the integer division a//b (Remember to import division from __future__) 
+The second line is the result of a mod b. (Modulo operator) 
+In the third line print the divmod of a and b.
+
+Input Format 
+The first line contains the first integer, a, and the second line contains the second integer, b.
+
+Output Format 
+Print the result as described above.
+
+Sample Input
+
+177
+10
+Sample Output
+
+17
+7
+(17, 7)
+
+"""
+
+def hr_divmod():
+	#from __future__ import division
+
+	a = int(raw_input("Type one integer: "))
+	b = int(raw_input("Type one integer: "))
+
+	print a//b
+	print a%b
+	print divmod(a,b)
+
+####Power - Mod Power####
+
+"""
+Task 
+You are given three integers; print two lines. 
+The first line should print pow(a,b), and the second line should print the result of pow(a,b,m).
+
+Input Format 
+The first line contains a, the second line contains b, and the third line contains m.
+
+Constraints 
+1=<a=<10 
+1=<b=<10 
+2=<m=<1000
+
+Sample Input
+
+3
+4
+5
+Sample Output
+
+81
+1
+"""
+
+def hr_powermod():
+	a = int(raw_input())
+	b = int(raw_input())
+	m = int(raw_input())
+
+	print pow(a,b)
+	print pow(a,b,m)
+
+
+####Integers Come In All Sizes####
+
+"""
+Task 
+Read four numbers, a, b, c, and d, and print the result of ab+cd.
+
+Input Format 
+Four numbers are given on four lines.
+
+Constraints 
+1=<a=<1000 
+1=<b=<1000 
+1=<c=<1000 
+1=<d=<1000
+
+Output Format 
+Print the result in one line.
+
+Sample Input
+
+9
+29
+7
+27
+Sample Output
+
+4710194409608608369201743232  
+Note that this result is bigger than 263−1 and hence won't fit in long long int of C++ or a 64-bit integer.
+"""
+def hr_allsizes():
+	a = int(raw_input())
+	b = int(raw_input())
+	c = int(raw_input())
+	d = int(raw_input())
+
+	print pow(a,b) + pow(c,d)
+
+
+####LOOPS####
+"""
+Task 
+Read an integer N. For all non-negative integers i<N, print i2. See the sample for details.
+
+Input Format 
+The first and only line contains N.
+
+Constraints 
+1<=N<=20
+
+Output Format 
+Print N lines, one corresponding to each i.
+
+Sample Input
+
+5
+Sample Output
+
+0
+1
+4
+9
+16
+"""
+def hr_loops():
+	n = int(raw_input("Type one integer: "))
+	i = 0
+	while i < n:
+	    print i**2
+	    i += 1
+
+
+####What's Your Name?####
+
+"""
+Problem Statement
+
+Let's learn the basics of Python! You are given the first name and the last name of a person. Your task is to read them and print the following:
+
+Hello firstname lastname! You just delved into python.
+It's that simple!
+
+In Python you can read a line as a string using
+
+s = raw_input()
+#here s reads the whole line.  
+Input Format The first line contains the first name, and the second line contains the last name.
+
+Constraints 
+The length of the first and last name ≤ 10.
+
+Output Format Print the output as mentioned above.
+
+Sample Input
+
+Guido
+Rossum
+Sample Output
+
+Hello Guido Rossum! You just delved into python.
+Concept The input read by the program is stored as a string data type. A string is a collection of characters.
+
+"""
+
+def hr_name():
+	firstname = raw_input("Type your firstname: ")
+	lastname = raw_input("Type your lastname: ")
+
+	print "Hello "+firstname, lastname+"! You just delved into python."
+
+###Interchange two numbers###
+
+"""
+Problem Statement
+
+You are given two integers. Store them into two variables and exchange them. Can it get any 
+easier! Make sure to use a tuple to do the task, rather than using any fancy logic. Print the two numbers.
+
+Input Format
+
+Two integers
+
+Output Format
+
+Two integers
+
+Sample Input
+
+2
+1
+Sample Output
+
+1
+2
+"""
+def reversenums():
+	a = raw_input("Type one integer: ")
+	b = raw_input("Type a second integer: ")
+
+	t = b,a
+
+	print t[0]
+	print t[1]
+
+####Finding the percentage####
+
+"""
+
+"""
+
+
+
+
+if __name__ == '__main__':
+    print dir()
