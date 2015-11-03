9bf783eb824bd1c50c082b3be803bfae1cb4bdd3
Vanesa <vanesa@gmx.net>
2015-11-02 16:08:19 -0800

    finish hr pythonintro

--- hackerrank/pythonintro.py
+++ hackerrank/pythonintro.py
@@ -310,10 +310,47 @@ def reversenums():
 ####Finding the percentage####
 
 """
+Problem Statement
 
+There is a record of 'n' students, each record having name of student, percent marks obtained in Maths, Physics and Chemistry. Marks can be floating values. The user enters an integer 'n' followed by names and marks for the 'n' students. You are required to save the record in a dictionary data type. The user then enters name of a student and you are required to print the average percentage marks obtained by that student, correct to two decimal places.
+
+Input Format
+
+Integer N followed by name and marks for N students, followed by the name of the particular student.
+
+Output Format
+
+Average percentage of marks obtained
+
+Constraints 
+2 <= N <= 10 
+0 <= Marks <= 100
+
+Sample Input
+
+3
+Krishna 67 68 69
+Arjun 70 98 63
+Malika 52 56 60
+Malika
+Sample Output
+
+56.00
 """
 
-
+def average_grade():
+	student_num = int(raw_input())
+	students = {}
+	for i in range(student_num):
+	    data = raw_input()
+	    student = data.split(" ")
+	    average = (float(student[1]) + float(student[2]) + float(student[3]))/3
+	    students[student[0]] = average
+	sel_student = raw_input()
+	if sel_student in students:
+	    print format(students[sel_student], '.2f')
+	else:
+	    print sel_student + "is not in the database"
 
 
 if __name__ == '__main__':
