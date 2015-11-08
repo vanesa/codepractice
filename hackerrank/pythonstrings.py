15c518441eedad7866198477d979711c94d9cd87
Vanesa <vanesa@gmx.net>
2015-11-07 19:37:45 -0800

    add string split and join challenge

--- hackerrank/pythonstrings.py
+++ hackerrank/pythonstrings.py
@@ -42,4 +42,45 @@ def swap_case('HackerRank.com presents "Pythonist 2".'):
 	        swapped = swapped + x.upper()
 	print swapped
 
+
+##### String Split and Join #####
+"""
+Problem Statement
+
+In Python a string can be split on a delimiter.
+
+Example
+
+>>> a = "this is a string"
+>>> a = a.split(" ") # a is converted to a list of strings. 
+>>> print a
+['this', 'is', 'a', 'string']
+Joining a string is simple
+
+>>> a = "-".join(a)
+>>> print a
+this-is-a-string 
+Task 
+You are given a string. Split the string on " " (space) delimiter and join using a - hyphen.
+
+Input Format 
+The first line contains a string consisting of words separated by space.
+
+Output Format 
+Print the formatted string as explained above.
+
+Sample Input
+
+this is a string   
+Sample Output
+
+this-is-a-string
+
+"""
+
+def split_join():
+	phrase = raw_input()
+	phrase = phrase.split()
+	print "-".join(phrase)
+
 	if __name__ == '__main__':
