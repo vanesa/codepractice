0f0bfc442bf69bfd3f1c319613f9fcd132dd9a2b
Vanesa <vanesa@gmx.net>
2015-11-19 22:58:39 -0800

    add codewars_js

--- codewars/codewars_js.js
+++ codewars/codewars_js.js
@@ -1,8 +1,21 @@
 // Codewars JS challenges 
 
-// Instructions:
+// roundToNext5 Instructions:
+// Given an integer as input, can you round it to the next 5?
 
+// Examples:
 
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// etc.
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
 
 
 function roundToNext5(n){
@@ -16,3 +29,4 @@ function roundToNext5(n){
   }
   return num
 }
+
